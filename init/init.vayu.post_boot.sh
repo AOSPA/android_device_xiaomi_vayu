37589fbf18faf71329809c80eeda57c9b1c9c5ea
Andres Oportus <andresoportus@google.com>
2017-03-24 11:21:47 -0700

    vayu: init: Add SchedTune configuration
    Bug: 36367381
    Test: Boot and check stune configuration effect on sysfs
    
    Change-Id: I838c31d164684a5d88550c92284b65bb50f2ed56
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- init/init.vayu.post_boot.sh
+++ init/init.vayu.post_boot.sh
@@ -72,6 +72,11 @@ echo 120 > /sys/module/cpu_boost/parameters/input_boost_ms
 echo "0:0 1:0 2:0 3:0 4:2323200 5:0 6:0 7:2323200" > /sys/module/cpu_boost/parameters/powerkey_input_boost_freq
 echo 400 > /sys/module/cpu_boost/parameters/powerkey_input_boost_ms
 
+# Configure default schedTune value for foreground/top-app
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 10 > /dev/stune/top-app/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+
 # Disable wsf, beacause we are using efk.
 # wsf Range : 1..1000 So set to bare minimum value 1.
 echo 1 > /proc/sys/vm/watermark_scale_factor
