7d91c09094a5ca9cc4f790fb1eb229164ded5ad2
Bruno Martins <bgcngm@gmail.com>
2018-04-27 00:04:21 +0100

    vayu: List additional native libraries to expose to apps
    Change-Id: I552dc44a32221d2d3cec48e1c64ed72c470d412e

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
