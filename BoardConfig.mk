cf02c654d0817e78a473aa51d9b3106a3bb9d94d
Ethan Chen <intervigil@gmail.com>
2019-02-03 15:12:39 -0800

    vayu: Disable verity and descriptor checking
    * avbtool takes a --flags argument, not --flag
    * avbtool provides an option to disable hashtree checking
    
    Change-Id: Ib57d8ebed69a0998005563a9268f9fc736cbeaf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,8 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # Inherit from proprietary files
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
