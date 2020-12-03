8646ecd69b0da69a79b9a23a5dbfd02b4cbea4c4
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    vayu: Disable the usage of ConfigStore
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- device.mk
+++ device.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
