6338699fb5987b07bcb306e82e67625e33850f07
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-20 17:21:39 +0530

    vayu: Relax refresh rate switching

--- properties/default.mk
+++ properties/default.mk
@@ -17,8 +17,8 @@
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.frame_rate_multiple_threshold=120 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.set_idle_timer_ms=80 \
-    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 # Enable app/sf phase offset as durations. The numbers below are translated from the existing
