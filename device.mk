9aaacda6bdaae9c841b0e29df6d4ab36e75b68fd
Marijn Suijten <marijns95@gmail.com>
2021-10-06 21:26:13 +0200

    vayu: Provide keymaster HAL on vendor
    In [1] Google removed many HIDL HAL libraries from VNDK, causing them to
    only be installed to the system partition.  This is inaccessible for
    vendor apps and results in the usual:
    
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/hw/android.hardware.keymaster@4.0-service-qti": library "android.hardware.keymaster@4.0.so" not found: needed by main executable
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/hw/android.hardware.keymaster@4.0-service-qti": library "android.hardware.keymaster@4.1.so" not found: needed by /odm/lib64/libqtikeymaster4.so in namespace (default)
    
    These libraries are listed in common-treble because they're used by ODM
    apps but have no explicit vendor dependency (ie. a binary placed on
    /vendor) to put them in a vendor-available space.  The default is to
    include system variants which is why the dependencies have to be
    suffixed with `.vendor` so that they're shipped in `/vendor` and are
    available for our ODM blobs even on GSIs.
    
    [1]: https://cs.android.com/android/_/android/platform/hardware/interfaces/+/d610435ac4bb051761b0f016aa6cdf2e884c55b5
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>
    Change-Id: I8344b85fb4e86a913bd8d9071ce49150ed093575

--- device.mk
+++ device.mk
@@ -158,6 +158,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
