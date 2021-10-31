6ce9726de4ade47684b091314c79e4437ec9b826
Maitreya29 <maitreyapatni30@gmail.com>
2021-10-31 11:23:07 +0530

    vayu: Switch to QTI BT
    Revert "vayu: audio: Include bluetooth_audio_policy_configuration instead of bluetooth_hearing_aid_audio_policy_configuration"
    
    This reverts commit cd955be6e2a562cc6f42bee7135ae3730e5c03f4.
    
    Revert "vayu: audio: Remove QTI BT A2DP routes"
    
    This reverts commit df2c33a38cd71925ef245d00e947565f24cdc054.
    
    Revert "Revert "vayu: BT: Switch to bdroid_buildcfg.h for qti bt stack""
    
    This reverts commit e9cc3a1af5f23e53ac664e8491ebf92437450c7d.
    
    Revert "vayu: Switch to AOSP BT stack"
    
    This reverts commit 0038a1b99a8937671e9e12bac2b270c3e4763cf5.
    
    Revert "vayu: Split component-overrides to support pure aosp/qti bt stack"
    
    This reverts commit 189ae1c65aa7f9c4cc49936f8b6762c6b0b533b2.

--- device.mk
+++ device.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
+    libbthost_if \
     libldacBT_dec \
     libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
@@ -89,9 +90,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
