f9a0049c03a2e39b8ef7bcec755876fe248825c2
raghavt20 <raghavt20@aospa.co>
2022-01-30 11:35:40 +0530

    vayu: Configure telephony services
    - Import Following Radio stacks from stock
     - CNE blobs and configs
     - DPM blobs
     - IMS blobs
     - RIL blobs
     - QMI blobs
    
    - Use QTI common telephony component
    
    Change-Id: I95df14fc9bb87174320a01db41cf2d21e359699a

--- device.mk
+++ device.mk
@@ -45,7 +45,8 @@ TARGET_BOARD_PLATFORM := msmnile
 # QTI components
 TARGET_COMMON_QTI_COMPONENTS := \
     bt \
-    overlay
+    overlay \
+    telephony
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
