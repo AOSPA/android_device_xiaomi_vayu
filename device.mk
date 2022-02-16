7d6dc6bbebd70981f22b39c70d1db0f844315569
Foxtrot47 <jjneutron@outlook.com>
2022-02-16 13:53:14 +0100

    vayu: Pull libwfdaac_vendor from stock

--- device.mk
+++ device.mk
@@ -318,8 +318,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor
-
 include vendor/xiaomi/vayu/vayu-vendor.mk
