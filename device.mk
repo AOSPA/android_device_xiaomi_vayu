e22d92867faf04811aaaba59d36a818056b4b618
Bruno Martins <bgcngm@gmail.com>
2022-05-02 11:28:36 +0100

    vayu: Use common fingerprint HIDL
    Change-Id: I7cb0a45c865d9dbfd800b1d633f8d5ae613bb1b9

--- device.mk
+++ device.mk
@@ -84,6 +84,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
