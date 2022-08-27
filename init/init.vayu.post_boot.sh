93ec7d3271178dc4252f2b708ba1c1aea3590c28
Jake Weinstein <jake@aospa.co>
2022-08-27 17:21:04 +0900

    vayu: init: Move background cpuset to CPU0-1
    Background tasks are not UX-critical.
    Free up cores to work on more important
    UX tasks.
    
    Change-Id: Ib0c8671ae56f377e78ba2d07e1a960489496c628
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- init/init.vayu.post_boot.sh
+++ init/init.vayu.post_boot.sh
@@ -35,7 +35,7 @@ echo 10 > /proc/sys/kernel/sched_group_downmigrate
 echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
 # cpuset parameters
-echo 0-2     > /dev/cpuset/background/cpus
+echo 0-1     > /dev/cpuset/background/cpus
 echo 0-3     > /dev/cpuset/system-background/cpus
 echo 4-7     > /dev/cpuset/foreground/boost/cpus
 echo 0-2,4-7 > /dev/cpuset/foreground/cpus
