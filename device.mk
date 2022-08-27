0f7ccf8a8ace2cbcd1ca07a191fc8f89457f918c
Foxtrot47 <jjneutron@outlook.com>
2022-08-27 21:54:55 +0530

    Revert "vayu: Pin Patched camera.qcom.so and libmegvii shim"
    This reverts commit d954a14bb4a70f49c3dfaafa8097c2979ae4a413.
    
    Change-Id: I2ab346d4aaba620fbc607400b9da83651da9022e

--- device.mk
+++ device.mk
@@ -88,6 +88,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libshim_megvii
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
