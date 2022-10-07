0841d29435787f8cf07b8e8f4a9217cab2f9a74b
UtsavBanar1231 <utsavbalar1231@gmail.com>
2022-10-07 17:10:32 +0200

    vayu: camera: Import camxoverride settings configuration.
    Change-Id: Iaf35f2bef15f742a3ea6d737ed19e2ec9fea81f9

--- device.mk
+++ device.mk
@@ -94,6 +94,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
 PRODUCT_SYSTEM_PROPERTIES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera \
     persist.vendor.camera.facepp.fdenable=0 \
