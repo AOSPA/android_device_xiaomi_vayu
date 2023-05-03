bed062bbcc02586c6f643fe9577cbc52786a98e6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-05-03 19:51:35 +0530

    vayu: Add MIUI camera support
    Change-Id: Ieb2590859f4739ae332760911c09c2f7934f8df6
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- init/init_vayu.cpp
+++ init/init_vayu.cpp
@@ -64,7 +64,7 @@ void set_ro_build_prop(const string &source, const string &prop,
 }
 
 void set_device_props(const string brand, const string device, const string model,
-                      const string name, const string manufacturer) {
+        const string name, const string manufacturer, const string mod_device) {
     // list of partitions to override props
     string source_partitions[] = {"", "bootimage.", "odm.", "product.",
                                   "system.", "system_ext.", "vendor."};
@@ -77,14 +77,15 @@ void set_device_props(const string brand, const string device, const string mode
         set_ro_build_prop(source, "name", name, true);
         set_ro_build_prop(source, "manufacturer", manufacturer, true);
     }
+    property_override("ro.product.mod_device", mod_device.c_str());
 }
 
 void vendor_load_properties() {
      // Detect device and configure properties
     if (GetProperty("ro.boot.hwc", "") == "INDIA") {
-        set_device_props("POCO", "bhima", "M2102J20SI", "bhima_in", "Xiaomi");
+        set_device_props("POCO", "bhima", "M2102J20SI", "bhima_in", "Xiaomi", "vayu_in_global");
     } else {
-        set_device_props("POCO", "vayu", "M2102J20SG", "vayu", "Xiaomi");
+        set_device_props("POCO", "vayu", "M2102J20SG", "vayu", "Xiaomi", "vayu_global");
     }
 
     // Set hardware revision
