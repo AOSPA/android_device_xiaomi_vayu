8ef0a8cbe0b1d9d3017fbc95794e9cbfb0bf796c
raghavt20 <raghavt20@aospa.co>
2023-05-15 22:56:22 +0530

    vayu: Include board-info.txt
    Change-Id: I2154d652c2fe6ce77c7906477af973751776dfe4
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a76
 # Assert
 TARGET_OTA_ASSERT_DEVICE := vayu,bhima
 
+# Board Info
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msmnile
 TARGET_NO_BOOTLOADER := true
